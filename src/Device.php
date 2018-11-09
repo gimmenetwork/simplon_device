b93d40207d937d937728dd9e6f92461f3c02cc09
Tino Ehrich <tino@bigpun.me>
2018-11-09 08:50:17 +0100

    added storage to cache results

--- src/Device.php
+++ src/Device.php
@@ -3,6 +3,7 @@
 namespace Simplon\Device;
 
 use Jaybizzle\CrawlerDetect\CrawlerDetect;
+use Simplon\Interfaces\StorageInterface;
 
 class Device implements DeviceInterface
 {
@@ -14,6 +15,11 @@ class Device implements DeviceInterface
     const TYPE_TABLET = 'TABLET';
     const TYPE_MOBILE = 'MOBILE';
 
+    const STORAGE_NAMESPACE = 'device-detection';
+    const STORAGE_KEY_MODEL = 'model';
+    const STORAGE_KEY_TYPE = 'type';
+    const STORAGE_KEY_BOT = 'bot';
+
     /**
      * @var \Mobile_Detect
      */
@@ -22,6 +28,10 @@ class Device implements DeviceInterface
      * @var CrawlerDetect
      */
     private $crawlerDetect;
+    /**
+     * @var StorageInterface|null
+     */
+    private $storage;
 
     /**
      * @param string|null $agent
@@ -35,10 +45,35 @@ class Device implements DeviceInterface
     }
 
     /**
+     * @return StorageInterface|null
+     */
+    public function getStorage(): ?StorageInterface
+    {
+        return $this->storage;
+    }
+
+    /**
+     * @param StorageInterface $storage
+     *
+     * @return Device
+     */
+    public function setStorage(StorageInterface $storage): Device
+    {
+        $this->storage = $storage;
+
+        return $this;
+    }
+
+    /**
      * @return string
      */
     public function getModel(): string
     {
+        if ($this->hasStorage() && $model = $this->fetchStoredValue(self::STORAGE_KEY_MODEL))
+        {
+            return $model;
+        }
+
         $model = self::MODEL_FALLBACK;
 
         if ($this->mobileDetect->isIOS())
@@ -51,6 +86,11 @@ class Device implements DeviceInterface
             $model = self::MODEL_ANDROID;
         }
 
+        if ($this->hasStorage())
+        {
+            $this->setStoredValue(self::STORAGE_KEY_MODEL, $model);
+        }
+
         return $model;
     }
 
@@ -59,6 +99,11 @@ class Device implements DeviceInterface
      */
     public function getType(): string
     {
+        if ($this->hasStorage() && $type = $this->fetchStoredValue(self::STORAGE_KEY_TYPE))
+        {
+            return $type;
+        }
+
         $type = self::TYPE_FALLBACK;
 
         if ($this->mobileDetect->isTablet())
@@ -70,6 +115,11 @@ class Device implements DeviceInterface
             $type = self::TYPE_MOBILE;
         }
 
+        if ($this->hasStorage())
+        {
+            $this->setStoredValue(self::STORAGE_KEY_TYPE, $type);
+        }
+
         return $type;
     }
 
@@ -78,7 +128,19 @@ class Device implements DeviceInterface
      */
     public function isBot(): bool
     {
-        return $this->crawlerDetect->isCrawler();
+        if ($this->hasStorage() && $isBot = $this->fetchStoredValue(self::STORAGE_KEY_BOT))
+        {
+            return $isBot;
+        }
+
+        $isBot = $this->crawlerDetect->isCrawler();
+
+        if ($this->hasStorage())
+        {
+            $this->setStoredValue(self::STORAGE_KEY_BOT, $isBot);
+        }
+
+        return $isBot;
     }
 
     /**
@@ -103,5 +165,36 @@ class Device implements DeviceInterface
     public function isTypeFallback(): bool
     {
         return !$this->isTypeMobile() && !$this->isTypeTablet();
+    }
+
+    /**
+     * @return bool
+     */
+    private function hasStorage(): bool
+    {
+        return $this->getStorage() !== null;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return mixed|null
+     */
+    private function fetchStoredValue(string $key)
+    {
+        return $this->getStorage()->get(self::STORAGE_NAMESPACE . '-' . $key);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed  $value
+     *
+     * @return Device
+     */
+    private function setStoredValue(string $key, $value): self
+    {
+        $this->getStorage()->set(self::STORAGE_NAMESPACE . '-' . $key, $value);
+
+        return $this;
     }
 }
